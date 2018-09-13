b7fbcd90798c9c7ab6091b5185a9ef11823aff5c
Ajmal Noushad <ajufhn@gmail.com>
2018-09-13 15:13:22 +0530

    Changed API URLs, new URL notifications

--- api/management/commands/scrap_data.py
+++ api/management/commands/scrap_data.py
@@ -1,36 +1,45 @@
 from django.core.management.base import BaseCommand
-from api.views import results_view
+from api.views import results_view, notifications_view, generic_view
 from api.models import Data
 from firebase_admin import messaging
 from api.fcm import get_tokens
 
 
+TYPES = ['results', 'notifications']
+
+
+def compare_and_handle(type):
+    # Scrap data from website
+    latest = generic_view(None, True, type)
+    
+    try:
+        # Get data stored in db
+        data = Data.objects.filter(type=type).latest()
+
+        # Compare both
+        if latest == data.json_data:
+            print("{}: No Change".format(type))
+        else:
+            data.json_data = latest
+            data.save()
+            print("{}: Changes detected, Sending notifications".format(type))
+            message = messaging.Message(
+                data={
+                    'title': 'New {}'.format(type),
+                    'body': 'New {} have been published!'.format(type)
+                },
+                topic=type
+            )
+            response = messaging.send(message)
+            print(response)
+    except Data.DoesNotExist:
+        print('error')
+        Data.objects.create(json_data=latest, type=type)
+
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        data = results_view(None, True)
-        latest = None
-        try:
-            latest = Data.objects.latest()
-            # Check for change
+        list(map(compare_and_handle, TYPES))
 
-            if data == latest.json_data:
-                self.stdout.write("No Change")
-            else:
-                # For now
-                latest.json_data = data
-                latest.save()
-                message = messaging.Message(
-                    data={
-                        'title': 'New Results',
-                        'body': 'New results have been announced!'
-                    },
-                    topic='results'
-                )
-                response = messaging.send(message)
-                print(response)
-                self.stdout.write('Changed, sending notifications')
-        except Data.DoesNotExist:
-            latest = Data.objects.create(json_data=data)
-
-
+        
--- api/urls.py
+++ api/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import results_view
+from api import views
 
 urlpatterns = [
-    path('', results_view, name='results'),
+    path('results', views.results_view, name='results'),
+    path('notifications', views.notifications_view, name='notifications'),
 ]
